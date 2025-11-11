3787754bdfd996120927a5157c7dec7bd4668b68
sabilafirdias <sabilafirdias@gmail.com>
2025-11-11 15:29:43 +0700

    membuat app.get

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const express = require('express')
+const path = require('path')
+const crypto = require('crypto')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json()) // <-- Tambahan: agar bisa menerima JSON
+
+// Variabel penyimpanan API Key sementara
+let myApiKey = null
+
+app.get('/test', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
